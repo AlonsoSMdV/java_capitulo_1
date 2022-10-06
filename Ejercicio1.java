ebb57a771ca6a5d00cbf28882cdccca14f0e0196
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-06 08:55:56 +0200

    First commit

---
+++ Ejercicio1.java
@@ -0,0 +1,12 @@
+/**
+* Muestra por pantalla tu nombre
+*
+* 
+*/
+public class Eercicio1 { // Clase principal
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Su nombre es: ");
+	String nombre = sc.next();
+}
+}
