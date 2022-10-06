474de0cb2d05e6bb1e45fbf6c957f3e7341d820c
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-06 09:11:53 +0200

    First commit

---
+++ Ejercicio2Java.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio1Java {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca su nombre: ");
+        String nombre = sc.next();
+        System.out.print("Introduzca su dirección: ");
+        String direccion = sc.next();
+        System.out.print("Introduzca su número de teléfono: ");
+        String telefono = sc.next();
+    }
+}
