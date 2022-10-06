b9521973630fad829a9cce7174bc66c76bcec781
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2022-10-06 09:23:05 +0200

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 /**
 * Muestra por pantalla tu nombre
 *
 * 
 */
-public class Eercicio1 { // Clase principal
+public class Ejercicio1 { // Clase principal
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.print("Su nombre es: ");
 	String nombre = sc.next();
+	System.out.print("Buenas "+nombre);
 }
 }
