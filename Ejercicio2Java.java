5bf61ba597808748e425a4b0aba98e5483760c54
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2022-10-06 09:21:05 +0200

    Update Ejercicio2Java.java

--- Ejercicio2Java.java
+++ Ejercicio2Java.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ejercicio1Java {
+public class Ejercicio2Java {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca su nombre: ");
