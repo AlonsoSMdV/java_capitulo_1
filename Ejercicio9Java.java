17cea3ce749482cbc67a4874a44e075ce382844e
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-10 20:09:45 +0200

    New commit

---
+++ Ejercicio9Java.java
@@ -0,0 +1,19 @@
+/**
+* 
+*
+* Hacer un dibujo
+* 
+* 
+* 
+* 
+*
+* 
+*
+*/
+public class Ejercicio9Java {
+public static void main(String[] args) {
+System.out.println("\n  ⁄|   | ");
+System.out.println("    \|___| ");
+
+}
+}
