7e07587d872c7c701170d5f47d6d4979718112c3
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-10 09:54:35 +0200

    NEw Commit

--- Ejercicio4Java.java
+++ Ejercicio4Java.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
+/**
+* 
+*
+*  Escribe un programa que muestre tu horario de clase
+* 
+*
+*/
 public class Ejercicio4Java {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+    System.out.println("Lunes\tMartes\tMiérc.\tJueves.\tViernes");
+    System.out.println("======\t=======\t======\t=======\t=======");
+    System.out.println("PROG\tSI\tED\tPROG \tFOL");
+    System.out.println("PROG\tSI\tED\tPROG\tFOL");
+    System.out.println("LM\tSI\tPROG\tLM\tFOL");
+    System.out.println("LM\tPROG\tPROG\tLM\tSI");
+    System.out.println("BBDD\tPROG\tBBDD\tBBDD\tSI");
+    System.out.println("BBDD\tED\tBBDD\tBBDD\tSI");
     }
-
 }
