7a9ead04c594989d8c152b6b74bd129bf37da075
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-10 20:17:06 +0200

    New Commit

---
+++ Ejercicio10Java.java
@@ -0,0 +1,22 @@
+/**
+* 
+*
+* Hacer un dibujo
+* 
+* 
+* 
+* 
+*
+* 
+*
+*/
+public class Ejercicio10Java {
+    public static void main(String[] args) {
+    String verde = "\033[0;32m";
+    String blanco = "\033[0;37m";
+    System.out.println(verde+"\n     |     | ");
+    System.out.println("    (|     | ");
+    System.out.println("     |_____| \n"+blanco);
+    
+    }
+    }
--- Ejercicio9Java.java
+++ Ejercicio9Java.java
@@ -12,8 +12,9 @@
 */
 public class Ejercicio9Java {
 public static void main(String[] args) {
-System.out.println("\n  ⁄|   | ");
-System.out.println("    \|___| ");
+System.out.println("\n     |     | ");
+System.out.println("    (|     | ");
+System.out.println("     |_____| \n");
 
 }
 }
