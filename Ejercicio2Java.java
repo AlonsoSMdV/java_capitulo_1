b71b60f2c50820acb648860d929b10cff9513cba
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-10 08:33:41 +0200

    Correct

--- Ejercicio2Java.java
+++ Ejercicio2Java.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 public class Ejercicio2Java {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca su nombre: ");
-        String nombre = sc.next();
-        System.out.print("Introduzca su dirección: ");
-        String direccion = sc.next();
-        String numero = sc.next();
-        System.out.println("Introduzca su número de teléfono: ");
-        String telefono = sc.next();
+        System.out.println("Alonso Sánchez Moreno de Vega");
+        System.out.println("Calle Eneas/58 ");
+        System.out.println("681634035 ");
     }
 }
