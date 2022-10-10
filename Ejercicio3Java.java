84dd6099aeb605ecc8129296908026b7d39dbf45
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-10 09:09:54 +0200

    New Commit

---
+++ Ejercicio3Java.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio3Java {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%-10s %s\n","computer","ordenador");
+        System.out.printf("%-10s %s\n","student","alumno/a");
+        System.out.printf("%-10s %s\n","cat","gato");
+        System.out.printf("%-10s %s\n","penguin","pingüino");
+        System.out.printf("%-10s %s\n","machine","máquina");
+        System.out.printf("%-10s %s\n","nature","naturaleza");
+        System.out.printf("%-10s %s\n","light","luz");
+        System.out.printf("%-10s %s\n","green","verde");
+        System.out.printf("%-10s %s\n","book","libro");
+        System.out.printf("%-10s %s\n","pyramid","pirámide");
+    }
+}
